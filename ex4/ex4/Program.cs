e88ee5174d7491233e0a46dd1a3cd4f171d08682
paulinhoferrarez <paulinhoferrarez2013@hotmail.com>
2018-09-26 17:32:27 -0300

    exercicios while

---
+++ ex4/ex4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4.	Faça um algoritmo que exiba todos os números, a quantidade
+             * de pares e a soma dos ímpares contidos no intervalo
+             * digitado pelo usuário;*/
+
+            Console.WriteLine("Exercicio 4");
+            Console.WriteLine();
+
+            Console.Write("Primeiro número do intervalo: "); //informa ao usuário o que ele deve digitar
+            int primeiroNumero = Convert.ToInt32(Console.ReadLine()); //lê, converte e guarda o valor digitado
+
+            Console.Write("ultimo número do intervalo: "); //informa ao usuário o que ele deve digitar
+            int ultimoNumero = Convert.ToInt32(Console.ReadLine()); //lê, converte e guarda o valor digitado
+
+            int qtdePares = 0; //declara a variavel com valor 0
+
+            int somaImpares = 0; //declara a variavel com valor 0
+
+            while (primeiroNumero <= ultimoNumero) //inicia o laço de repetiçao enquanto a variavel primeiroNumero for menor ou igual ao ultimoNumero
+            {
+                Console.WriteLine(primeiroNumero); //imprime na tela o valor da variavel primeiroNumero
+                if (primeiroNumero % 2 == 0) //condiçao que detecta os numeros pares
+                {
+                    qtdePares = qtdePares + 1; //soma +1 a quantidade de pares
+                }
+                else //condiçao que detecta os numeros impares
+                {
+                    somaImpares = somaImpares + primeiroNumero; //adiciona o numero impar na soma dos impares
+                }
+                primeiroNumero = primeiroNumero + 1; //adiciona +1 a variavel primeiroNumero
+            }
+            Console.WriteLine("Quantidade de pares: " + qtdePares); //informa a quantidade de numeros pares
+            Console.WriteLine("Soma dos impares: " + somaImpares); //informa a soma dos numeros impares
+            Console.ReadKey(); //encerramento do programa
+        }
+    }
+}
---
+++ ex5/ex5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*5.	Faça um algoritmo que leia um valor N e imprima a soma de todos os números pares
+             * entre 1 (inclusive) e N (inclusive). Considere que o N será sempre maior que ZERO.*/
+
+            Console.WriteLine("Exercicio 5");
+            Console.WriteLine();
+
+            Console.Write("Ultimo número do intervalo: "); //informa ao usuário o que ele deve digitar
+            int ultimoNumero = Convert.ToInt32(Console.ReadLine()); //lê, converte e guarda o valor digitado
+
+            int primeiroNumero = 1; //declara a variavel com valor 1
+            int somaPares = 0; //declara a variavel com valor 0
+
+            while (primeiroNumero <= ultimoNumero) //inicia o laço de repetiçao enquanto a variavel primeiroNumero for menor ou igual ao ultimoNumero
+            {
+                if (primeiroNumero % 2 == 0) //condiçao que detecta os numeros pares
+                {
+                    somaPares = somaPares + primeiroNumero; //adiciona o numero par a soma dos pares
+                }
+                primeiroNumero = primeiroNumero + 1; //adiciona +1 a variavel primeiroNumero
+            }
+            Console.WriteLine();
+            Console.WriteLine("A soma dos números pares é: " + somaPares); //informa a soma dos numeros pares
+            Console.ReadKey(); //encerramento do programa
+        }
+    }
+}
